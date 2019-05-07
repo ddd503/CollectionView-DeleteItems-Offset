988deca26ee3f2441fc72c2f3bb5055edc22d60d
ddd503 <skbsporta7@gmail.com>
2019-05-07 23:39:16 +0900

    Initial commit

---
+++ CollectionView-DeleteItems-Offset/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionView-DeleteItems-Offset
+//
+//  Created by kawaharadai on 2019/05/07.
+//  Copyright © 2019 kawaharadai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
